cc0df40643d9bc9b876ff07218c638c6a0c58ce8
CLV <284387@studenti.unimore.it>
2023-08-26 19:13:55 +0200

    first commit

---
+++ venv/parte1.py
@@ -0,0 +1,5 @@
+#voglio salvare continuamente lo stato di questo progetto
+#vcs-->create git repositery (vedo che il file diventa giallo)
+#giallo perchè non ancora memorizzato, quindi
+#clic destro si parte1, git, add
+#per salvarlo, git-->commit
